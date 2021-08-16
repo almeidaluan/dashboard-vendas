ea2f3c095b4dbca2cb109ed6ad310b982031da78
Luan Almeida <almeida.luan099@gmail.com>
2021-08-16 19:09:51 -0300

    feature: Estrutura inicial do projeto Front e Back

---
+++ dsvendas/src/test/java/com/company/dsvendas/DsvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.dsvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
