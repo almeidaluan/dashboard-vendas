0e3dccc24a776a3ed26987bbab3e40bd23b18e2e
Luan Almeida <almeida.luan099@gmail.com>
2021-08-16 22:22:28 -0300

    feature: Adicionando Bootstrap e CSS customizado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,9 +4,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello World
-        </p>
+        <div>
+          <h1>Hello World</h1>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
